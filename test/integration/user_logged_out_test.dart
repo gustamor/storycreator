d7687dbb8adf038ac74a1243f029363d67315bc3
Gustavo Moreno <gustavo.moreno.fernandez@gmail.com>
2024-03-14 16:16:42 +0100

    add logout button

---
+++ test/integration/user_logged_out_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:story_creator/ui/layouts/main/screens/main_portrait_screen.dart';
+
+void main() {
+  group('logout process', () {
+    testWidgets('should navigatoe to signin screen when log out', (tester) async {
+       await tester.pumpWidget(
+      const ProviderScope(
+        child: CupertinoApp(
+          home: ScreenUtilInit(
+            child: MainPortraitScreen(),
+          ),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+        
+
+    });
+
+  });
+}
